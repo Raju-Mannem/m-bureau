750f3ab1141059371a89160a54c6a7fd589f1722
Raju Mannem <rajumannem71@gmail.com>
2024-08-26 12:17:08 +0530

    components configured

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@ import { useState } from 'react'
 import './App.css'
 import Main from './components/Main'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Main />
     </>
   )
 }
-
 export default App

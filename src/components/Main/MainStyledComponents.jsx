1c9d48103fde84ade15caf08a0384baca5420f46
Raju Mannem <rajumannem71@gmail.com>
2024-08-26 13:06:43 +0530

    components router

---
+++ src/components/Main/MainStyledComponents.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.div`
+min-height:90vh;
+width:95vw; 
+`
+export const DBHome = styled.div`
+display:flex;
+`
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Contact from './Contact'
+import Disclimar from '../Disclimar'
+import Home from './Home'
+import About from '../About'
 
-function Main() {
+const Main=()=> {
+    const location = useLocation()
   return (
-    <div>Main</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/disclimar" element={<Disclimar />} />
+      </Routes>
+    </div>
   )
 }
 

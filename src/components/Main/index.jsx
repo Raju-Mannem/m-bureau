07ac11ef71cf3a533905510a94e603980fe22ae8
Raju Mannem <rajumannem71@gmail.com>
2024-08-27 17:31:28 +0530

    tailwind css configuration 2

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Routes, Route, useLocation } from 'react-router-dom'
-import Contact from './Contact'
+import Contact from '../Contact'
 import Disclimar from '../Disclimar'
-import Home from './Home'
+import Home from '../Home'
 import About from '../About'
-
+import Login from '../Login'
 const Main=()=> {
     const location = useLocation()
   return (
@@ -14,6 +14,7 @@ const Main=()=> {
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/disclimar" element={<Disclimar />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   )

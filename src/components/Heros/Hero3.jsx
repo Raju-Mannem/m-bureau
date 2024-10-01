aa7795701b7adbbc6cc00b95d1aea64eb256da83
Raju Mannem <rajumannem71@gmail.com>
2024-10-01 18:15:35 +0530

    heros u

--- src/components/Heros/Hero3.jsx
+++ src/components/Heros/Hero3.jsx
@@ -25,10 +25,8 @@ function Hero3() {
         animate={isVisible ? { y: -20, opacity: 1 } : {}}
         transition={{ duration: 2, ease: 'linear' }}
       >
-        <div className="mx-auto max-w-screen-xl px-4 py-32 sm:px-6 sm:py-12 lg:px-8 lg:py-8 shadow-2xl shadow-yellow-950 rounded-lg
-        bg-[url('https://res.cloudinary.com/dhxtw97su/image/upload/f_auto,q_auto/v1/marriage-bureau/evbsmlx59bl8dwpn9fi9')]
-        bg-no-repeat
-        bg-cover
+        <div className="mx-auto mb-20 max-w-screen-xl px-4 py-32 sm:px-6 sm:py-12 lg:px-8 lg:py-8 shadow-2xl shadow-yellow-950 rounded-lg
+        bg-white
         ">
           <div className="grid sm:grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 lg:py-8">
             <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">

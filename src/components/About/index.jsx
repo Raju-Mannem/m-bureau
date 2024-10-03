9b3e1fa4f54841737d831567a3522b32200cf68e
Raju Mannem <rajumannem71@gmail.com>
2024-10-03 18:27:03 +0530

    contact u

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,92 +1,52 @@
 import React from "react";
 import { NavbarDemo } from "../Navbar";
-import { Link } from "react-router-dom";
 import Footer from "../Footer";
+import About1 from "./About1";
+import About2 from "./About2";
+import About3 from "./About3";
 import "@fontsource/ramabhadra";
 import "@fontsource/ramaraja";
 import "./index.css";
-
 function About() {
   return (
     <div>
       <NavbarDemo />
-      <div>
-        <div className="container m-auto py-32 px-6 text-gray-600 md:px-12 xl:px-6 bg-slate-50">
-          <h2 className="text-2xl text-[#00bf63] font-bold md:text-4xl">
+      <div className="bg-[url('https://img.freepik.com/premium-photo/simple-elegant-green-leaves-dots-background_1007521-19543.jpg?ga=GA1.1.268735415.1727945180&semt=ais_hybrid')]
+      bg-no-repeat
+      bg-cover
+      bg-center
+      ">
+        <div className="container m-auto py-32 px-6 text-gray-600 md:px-12 xl:px-6 ">
+          <h2 className="text-2xl text-[#00bf63] font-bold md:text-4xl pb-8">
             <strong className="underline">కృష్ణవేణి మ్యారేజ్ బ్యూరో</strong>,
             సంప్రదాయాలను గౌరవించి, సంతోషకరమైన సంబంధాలను ఏర్పరుస్తాము
           </h2>
-          <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12 py-12">
-            <div className="md:5/12 lg:w-5/12">
-              <img
-                src="https://res.cloudinary.com/dhxtw97su/image/upload/f_auto,q_auto/v1/marriage-bureau/ecg2rsekjf9pnpeqakbk"
-                alt="image"
-                loading="lazy"
-                width=""
-                height=""
-              />
-            </div>
-            <div className="md:7/12 lg:w-6/12 text-left leading-relaxed">
-              కృష్ణవేణి మ్యారేజ్ బ్యూరో వద్ద నమ్మకమైన మరియు వ్యక్తిగత సేవలను
-              అందించడానికి సిద్ధంగా ఉన్నాను.
-              <h1> మా కథ </h1>
-              మీరు సరైన భాగస్వామిని కనుగొనడంలో మీకు సహాయపడటమే మా ముఖ్యమైన
-              లక్ష్యం! ప్రతి ప్రొఫైల్‌ను పరిశీలించడం, మీరు కోరుకునే విలువలు
-              మరియు ఆశయాలను అర్థం చేసుకోవడం ద్వారా, మీకు సరిపోయే సంబంధాలను
-              ఏర్పరచడం మా సర్వస్వం.
-              <h1> మా ప్రత్యేకతలు </h1>
-              <ul>
-                <li>
-                  <strong>సాంస్కృతిక సెన్సిటివిటీ:</strong> తెలుగు కుటుంబాలకు
-                  ప్రత్యేకమైన సాంస్కృతిక నైపుణ్యాలను గౌరవిస్తూ, మా సేవలు ఈ
-                  సంప్రదాయాలకు అనుగుణంగా రూపొందించబడ్డాయి.
-                </li>
-                <li>
-                  <strong>వ్యక్తిగత సేవ:</strong> మీకు ప్రత్యేకమైన అవసరాలు
-                  ఉన్నాయని మేము అర్థం చేసుకుంటాము. అందుకే, మీకు సరైన భాగస్వామిని
-                  కనుగొనడం సులభంగా మరియు ఆనందంగా ఉండేలా మేము కృషి చేస్తాము.
-                </li>
-                <li>
-                  <strong>గోప్యత:</strong> మీ వ్యక్తిగత సమాచారాన్ని మేము అత్యంత
-                  గోప్యతతో నిర్వహిస్తాము. మీ నమ్మకం మాకు చాలా విలువైనది.
-                </li>
-                <li>
-                  <strong>విశ్లేషణ మార్గనిర్దేశం:</strong> మా అనుభవంతో, మేము
-                  మీకు ఉత్తమ మార్గనిర్దేశాన్ని అందించగలము, ఇది మీ కోసం సరైన
-                  భాగస్వామిని కనుగొనడంలో సహాయపడుతుంది.
-                </li>
-              </ul>
-              <h1>మా హామీ</h1>
-              <p>
-                మీ భాగస్వామిని కనుగొనడం మీకు సంతృప్తికరమైన అనుభవంగా
-                మారాలనుకుంటున్నాము. మీ సంప్రదాయాలను గౌరవించే మరియు మీ ఆశయాలను
-                నెరవేర్చే స్ఫూర్తితో, మేము మీకు సహాయపడేందుకు సిద్ధంగా ఉన్నాం.
-              </p>
-              <h1>మాతో సంప్రదించండి</h1>
-              <p>
-                మీరు మమ్మల్ని సంప్రదించి, మా సేవలను గురించి తెలుసుకోవడానికి
-                స్వాగతం!
-              </p>
-              <ul className="leading-6 my-6">
-                <li className="leading-6">
-                  <strong>ఫోన్:</strong> +91 1111111111
-                </li>
-                <li className="leading-6">
-                  <strong>ఇమెయిల్:</strong><Link to="mailto:abc@gmail.com">abc@gmail.com</Link>
-                </li>
-                <li className="leading-6">
-                  <strong>చిరునామా:</strong> మీ ఆఫీసు చిరునామా
-                </li>
-              </ul>
-              <p>
-                <strong>
-                  కృష్ణవేణి మ్యారేజ్ బ్యూరో"ని మీ అనుభవంలో భాగస్వామిగా ఎంపిక
-                  చేసినందుకు ధన్యవాదాలు. మీ సంతోషకరమైన జీవితాన్ని కనుగొనడానికి
-                  మేము ఎదురు చూస్తున్నాము!
-                </strong>
-              </p>
-            </div>
-          </div>
+          <p>
+            కృష్ణవేణి మ్యారేజ్ బ్యూరో వద్ద నమ్మకమైన మరియు వ్యక్తిగత సేవలను
+            అందించడానికి సిద్ధంగా ఉన్నాను.
+          </p>
+          <About1 />
+          <span className="relative flex justify-center">
+            <div className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
+
+            <span className="relative z-10 bg-white px-6">
+              Lorem, ipsum dolor
+            </span>
+          </span>
+          <About2 />{" "}
+          <span className="relative flex justify-center">
+            <div className="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
+
+            <span className="relative z-10 bg-white px-6">
+              Lorem, ipsum dolor
+            </span>
+          </span>
+          <About3 />
+          <h2 className="text-2xl text-[#00bf63] font-bold md:text-4xl pb-8">
+            కృష్ణవేణి మ్యారేజ్ బ్యూరో"ని మీ అనుభవంలో భాగస్వామిగా ఎంపిక
+            చేసినందుకు ధన్యవాదాలు. మీ సంతోషకరమైన జీవితాన్ని కనుగొనడానికి మేము
+            ఎదురు చూస్తున్నాము!
+          </h2>
         </div>
       </div>
       <Footer />
--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -17,7 +17,7 @@ function Contact() {
         <div className="max-w-md mx-auto my-10 bg-white dark:bg-gray-800 p-5 rounded-md shadow-sm">
           <div className="text-center">
             <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">Contact Us</h1>
-            <p className="text-gray-400 dark:text-gray-400">Fill up the form below to send us a message.</p>
+            <p className="text-gray-400 dark:text-gray-400">సంప్రదించుటకు క్రింది ఫోరం ని ఆనుసరించండి  </p>
           </div>
           <div className="m-7">
             <form action="https://api.web3forms.com/submit" method="POST" id="form" onSubmit={handleSubmit}>
@@ -26,45 +26,45 @@ function Contact() {
               <input type="checkbox" name="botcheck" id="" style={{ display: 'none' }} />
 
               <div className="mb-6">
-                <label htmlFor="name" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Full Name</label>
+                <label htmlFor="name" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">పూర్తి  పేరు</label>
                 <input 
                   type="text" 
                   name="name" 
                   id="name" 
-                  placeholder="John Doe" 
+                  placeholder="మీ పేరును ఎంటర్ చేయండి " 
                   required 
                   className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" 
                 />
               </div>
               <div className="mb-6">
-                <label htmlFor="email" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email Address</label>
+                <label htmlFor="email" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">ఈమెయిల్ అడ్రసు </label>
                 <input 
                   type="email" 
                   name="email" 
                   id="email" 
-                  placeholder="you@company.com" 
+                  placeholder="మీ ఈమెయిల్ అడ్రసు ఎంటర్ చేయండి " 
                   required 
                   className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" 
                 />
               </div>
               <div className="mb-6">
-                <label htmlFor="phone" className="text-sm text-gray-600 dark:text-gray-400">Phone Number</label>
+                <label htmlFor="phone" className="text-sm text-gray-600 dark:text-gray-400">ఫోన్ నెంబర్ </label>
                 <input 
                   type="text" 
                   name="phone" 
                   id="phone" 
-                  placeholder="+1 (555) 1234-567" 
+                  placeholder="మీ ఫోన్ నెంబర్ ఎంటర్ చేయండి " 
                   required 
                   className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" 
                 />
               </div>
               <div className="mb-6">
-                <label htmlFor="message" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">Your Message</label>
+                <label htmlFor="message" className="block mb-2 text-sm text-gray-600 dark:text-gray-400">సందేశం</label>
                 <textarea 
                   rows="5" 
                   name="message" 
                   id="message" 
-                  placeholder="Your Message" 
+                  placeholder="మీ సందేశం ఎంటర్ చేయండి " 
                   className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500" 
                   required 
                 />
@@ -72,9 +72,9 @@ function Contact() {
               <div className="mb-6">
                 <button 
                   type="submit" 
-                  className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
+                  className="w-full px-3 py-4 text-white bg-[#00bf63] rounded-md focus:bg-indigo-600 focus:outline-none"
                 >
-                  Send Message
+                  సందేశం పంపండి
                 </button>
               </div>
               <p className="text-base text-center text-gray-400" id="result"></p>

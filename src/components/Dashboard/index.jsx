f982ce339111c8e62778662f62ed76ed9a41fec6
Raju Mannem <rajumannem71@gmail.com>
2024-10-05 18:02:01 +0530

    u3

---
+++ src/components/Dashboard/index.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Welcome to the Dashboard!</h1>
+      {/* Add more dashboard content here */}
+    </div>
+  );
+};
+
+export default Dashboard;

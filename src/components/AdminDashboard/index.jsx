97d342e686563d944d4f75cb039aae69925b9695
Raju Mannem <rajumannem71@gmail.com>
2024-10-11 20:13:13 +0530

    admin dashboard table responsiveness

--- src/components/AdminDashboard/index.jsx
+++ src/components/AdminDashboard/index.jsx
@@ -61,49 +61,47 @@ const AdminDashboard = () => {
             Logout
           </button>
         </div>
-        <div className="py-20 border-t border-green-200 overflow-hidden">
+        <div className="py-20 border-t border-green-200 overflow-auto">
           {error && <p className="text-red-500">{error}</p>}
-          <div className="overflow-hidden">
-            <table className="min-w-full table-auto bg-white border border-gray-300 text-sm">
-              <thead>
-                <tr className="bg-green-400 text-white text-sm">
-                  <th className="border px-4 py-2">User ID</th>
-                  <th className="border px-4 py-2">Email</th>
-                  <th className="border px-4 py-2">Payment Status</th>
-                  <th className="border px-4 py-2">Profile Access</th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.length > 0 ? (
-                  users.map((user) => (
-                    <tr key={user._id}>
-                      <td className="border px-4 py-2 text-xs">{user._id}</td>
-                      <td className="border px-4 py-2 text-xs">{user.email}</td>
-                      <td className="border px-4 py-2 text-xs">
-                        {user.payment ? "Paid" : "Unpaid"}
-                      </td>
-                      <td className="border px-4 py-2 text-xs">
-                        <label>
-                          <input
-                            type="checkbox"
-                            checked={user.access}
-                            onChange={() => toggleAccess(user._id, user.access)}
-                          />
-                          {user.access ? " Access" : " No Access"}
-                        </label>
-                      </td>
-                    </tr>
-                  ))
-                ) : (
-                  <tr>
-                    <td colSpan="4" className="border px-4 py-2 text-center">
-                      No users found
+          <table className="min-w-full bg-white border border-gray-300 text-sm">
+            <thead>
+              <tr className="bg-green-400 text-white text-sm">
+                <th className="border px-4 py-2">User ID</th>
+                <th className="border px-4 py-2">Email</th>
+                <th className="border px-4 py-2">Payment Status</th>
+                <th className="border px-4 py-2">Profile Access</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.length > 0 ? (
+                users.map((user) => (
+                  <tr key={user._id}>
+                    <td className="border px-4 py-2 text-xs">{user._id}</td>
+                    <td className="border px-4 py-2 text-xs">{user.email}</td>
+                    <td className="border px-4 py-2 text-xs">
+                      {user.payment ? "Paid" : "Unpaid"}
+                    </td>
+                    <td className="border px-4 py-2 text-xs">
+                      <label>
+                        <input
+                          type="checkbox"
+                          checked={user.access}
+                          onChange={() => toggleAccess(user._id, user.access)}
+                        />
+                        {user.access ? " Access" : " No Access"}
+                      </label>
                     </td>
                   </tr>
-                )}
-              </tbody>
-            </table>
-          </div>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="4" className="border px-4 py-2 text-center">
+                    No users found
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
         </div>
         <Footer />
       </div>

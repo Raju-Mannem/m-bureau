1635f783f1377848c3e1393c08fd0178ebea8995
Raju Mannem <rajumannem71@gmail.com>
2024-10-11 19:26:03 +0530

    admin config update

---
+++ src/components/AdminDashboard/index.jsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { NavbarDemo } from "../Navbar";
+import {Link} from "react-router-dom";
+import Footer from "../Footer";
+import axios from "axios";
+
+const AdminDashboard = () => {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedUsers = localStorage.getItem("users");
+    if (storedUsers) {
+      setUsers(JSON.parse(storedUsers));
+    } else {
+      setError("No users found in localStorage");
+    }
+  }, []);
+
+  const storedAdminName = localStorage.getItem("adminName");
+  if (!storedAdminName) {
+    navigate("/admin-signin");
+  }
+
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate("/admin-signin");
+  };
+
+  const toggleAccess = async (userId, currentAccess) => {
+    try {
+      const updatedAccess = !currentAccess;
+      const adminName = localStorage.getItem("adminName");
+        await axios.patch(`https://krishnaveni-marriagebureau.vercel.app/users/${userId}`, { access: updatedAccess, admin: adminName });
+        setUsers((prevUsers) =>
+        prevUsers.map((user) =>
+          user._id === userId ? { ...user, access: updatedAccess } : user
+        )
+      );
+    } catch (error) {
+      console.error("Error updating access:", error);
+      setError("Failed to update access");
+    }
+  };
+
+  return (
+    <div className="flex flex-row items-center justify-center w-screen h-full mx-2">
+      <div className="h-full w-screen">
+        <NavbarDemo />
+        <div className="h-full mt-28 mb-2 flex items-center justify-around dark:bg-gray-900 rounded-lg py-8">
+          <p className="sm:text-xs lg:text-sm">
+            Welcome: <strong className="text-green-500">{storedAdminName}</strong>
+          </p>
+          <Link to="/profiles" className="bg-green-400 text-xs text-white px-4 py-1 rounded font-bold focus:outline-none">view profiles</Link>
+          <button
+            onClick={handleLogout}
+            className="bg-green-400 hover:bg-green-200 focus:outline-none text-white sm:text-xs lg:text-sm font-bold py-1 px-4 rounded"
+          >
+            Logout
+          </button>
+        </div>
+        <div className="py-20 border-t border-green-200">
+          {error && <p className="text-red-500">{error}</p>}
+          <table className="min-w-full bg-white border border-gray-300 text-sm">
+            <thead>
+              <tr className="bg-green-400 text-white text-sm">
+                <th className="border px-4 py-2">User ID</th>
+                <th className="border px-4 py-2">Email</th>
+                <th className="border px-4 py-2">Payment Status</th>
+                <th className="border px-4 py-2">Profile Access</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.length > 0 ? (
+                users.map((user) => (
+                  <tr key={user._id}>
+                    <td className="border px-4 py-2 text-xs">{user._id}</td>
+                    <td className="border px-4 py-2 text-xs">{user.email}</td>
+                    <td className="border px-4 py-2 text-xs">
+                      {user.payment ? "Paid" : "Unpaid"}
+                    </td>
+                    <td className="border px-4 py-2 text-xs">
+                      <label>
+                        <input
+                          type="checkbox"
+                          checked={user.access}
+                          onChange={() => toggleAccess(user._id, user.access)}
+                        />
+                        {user.access ? " Access" : " No Access"}
+                      </label>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="4" className="border px-4 py-2 text-center">
+                    No users found
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
---
+++ src/components/Adminsignin/index.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { NavbarDemo } from "../Navbar";
+import Footer from "../Footer";
+import axios from "axios";
+import "@fontsource/roboto";
+import "@fontsource-variable/noto-sans-jp";
+import { useNavigate } from "react-router-dom";
+
+function Adminsignin() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    adminName: "",
+    adminPassword: "",
+  });
+  const [error, setError] = useState(null);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://krishnaveni-marriagebureau.vercel.app/api/adminlogin",
+        formData
+      );
+      const { adminName, users } = response.data;
+      localStorage.setItem('adminName', adminName);
+      localStorage.setItem('users', JSON.stringify(users));
+      navigate("/admin-dashboard");
+    } catch (error) {
+      setError("Invalid credentials, please try again.");
+    }
+  };
+  return (
+    <div>
+      <NavbarDemo />
+      <div>
+        <div className="flex items-center min-h-screen bg-gray-50 dark:bg-gray-900">
+          <div className="container mx-auto">
+            <div className="max-w-md mx-auto my-20 bg-white dark:bg-gray-800 p-5 rounded-md shadow-sm">
+              <div className="text-center">
+                <h1 className="my-3 sm:text-xl lg:text-3xl font-semibold text-gray-700 dark:text-gray-200">
+                  Admin Login
+                </h1>
+              </div>
+              <div className="m-7">
+                <form onSubmit={handleSubmit}>
+                  <div className="mb-6 flex items-center">
+                    <label
+                      htmlFor="adminName"
+                      className="mr-2 text-xs text-gray-600 dark:text-gray-400 w-1/3"
+                    >
+                      Admin Name:
+                    </label>
+                    <input
+                      type="text"
+                      name="adminName"
+                      id="adminName"
+                      value={formData.adminName}
+                      onChange={handleChange}
+                      placeholder="Enter your admin name"
+                      required
+                      className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-green-400 focus:border-green-400 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                    />
+                  </div>
+                  <div className="mb-6 flex items-center">
+                    <label
+                      htmlFor="adminPassword"
+                      className="mr-2 text-xs text-gray-600 dark:text-gray-400 w-1/3"
+                    >
+                      Password:
+                    </label>
+                    <input
+                      type="password"
+                      name="adminPassword"
+                      id="adminPassword"
+                      value={formData.adminPassword}
+                      onChange={handleChange}
+                      placeholder="Enter your admin password"
+                      required
+                      className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-green-400 focus:border-green-400 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+                    />
+                  </div>
+                  <div className="mb-6">
+                    <button
+                      type="submit"
+                      className="w-full px-3 py-4 text-white bg-[#00bf63] rounded-md text-xs focus:bg-indigo-600 focus:outline-none"
+                    >
+                      Login
+                    </button>
+                  </div>
+                  {error && <p className="text-red-500 text-xs">{error}</p>}
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Adminsignin;

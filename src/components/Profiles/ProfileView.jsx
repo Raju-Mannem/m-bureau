c7a62f354224923ce75cbfab4048170fb8440570
Raju Mannem <rajumannem71@gmail.com>
2024-10-20 03:30:24 +0530

    profiles delete and ui update

--- src/components/Profiles/ProfileView.jsx
+++ src/components/Profiles/ProfileView.jsx
@@ -12,7 +12,9 @@ function ProfileView() {
   useEffect(() => {
     const fetchProfile = async () => {
       try {
-        const response = await axios.get(`https://m-bureau-backend.onrender.com/api/profiles/${id}`);
+        const response = await axios.get(
+          `https://m-bureau-backend.onrender.com/api/profiles/${id}`
+        );
         setProfile(response.data);
       } catch (error) {
         setError(error.response?.data?.error || "An error occurred");
@@ -22,35 +24,97 @@ function ProfileView() {
   }, [id]);
   return (
     <div className="flex flex-row items-center justify-center w-screen h-full mx-2">
-      <div className="h-full w-screen
-      bg-[url('https://res.cloudinary.com/dhxtw97su/image/upload/f_auto,q_auto/v1/marriage-bureau/ls2vpjzhqd6qgfkmb96j')]
-      bg-no-repeat
-      bg-cover
-      bg-center">
+      <div
+        className="h-full w-screen">
         <NavbarDemo />
-        <div className="my-20 py-12">
+        <div className="my-12 py-12">
           {!profile ? (
             <div className="text-red-400">{error}</div>
           ) : (
-            <div className="rounded-lg p-8 text-left">
-            <h1 className="text-xl font-semibold mb-4">Profile Details</h1>
-              <p>Full Name: {profile.fullName}</p>
-              <p>Father Name: {profile.fatherName}</p>
-              <p>Mother Name: {profile.motherName}</p>
-              <p>Mobile: {profile.mobile}</p>
-              <p>Age: {profile.age}</p>
-              <p>Occupation: {profile.occupation}</p>
-              <p>Experience: {profile.experience}</p>
-              <p>Salary: {profile.salary}</p>
-              <p>Current Address: {profile.currentAddress}</p>
-              <p>Permanent Address: {profile.permanentAddress}</p>
-              <p>Photo 1: <img src={profile.photo1} alt="Profile Photo 1" className="w-32 h-32 object-cover" /></p>
-              <p>Photo 2: <img src={profile.photo2} alt="Profile Photo 2" className="w-32 h-32 object-cover" /></p>
-              <p>Height: {profile.height}</p>
-              <p>Message: {profile.message}</p>
+            <div className="rounded-lg p-8 bg-white text-left mx-auto sm:w-full lg:w-1/2 shadow-2xl">
+              <h1 className="text-xs lg:text-sm font-semibold pb-4 text-center border-b">
+                Profile Details
+                <img
+                  src="https://res.cloudinary.com/dhxtw97su/image/upload/f_auto,q_auto/v1/marriage-bureau/xy4pnmommssiuorbghoe"
+                  alt="Krishnaveni Marriage Bureau"
+                  className="w-auto h-10 inline-block px-4 mx-4 border-l"
+                />
+              </h1>
+              <table className="min-w-full">
+                <tbody>
+                  <tr className="border-b text-xs lg:text-sm">
+                    <td className="py-2 px-4 font-semibold">Full Name:</td>
+                    <td className="py-2 px-4">{profile.fullName}</td>
+                  </tr>
+                  <tr className="border-b text-xs lg:text-sm">
+                    <td className="py-2 px-4 font-semibold">Father Name:</td>
+                    <td className="py-2 px-4">{profile.fatherName}</td>
+                  </tr>
+                  <tr className="border-b text-xs lg:text-sm">
+                    <td className="py-2 px-4 font-semibold">Mother Name:</td>
+                    <td className="py-2 px-4">{profile.motherName}</td>
+                  </tr>
+                  <tr className="border-b text-xs lg:text-sm">
+                    <td className="py-2 px-4 font-semibold">Mobile:</td>
+                    <td className="py-2 px-4">{profile.mobile}</td>
+                  </tr>
+                  <tr className="border-b text-xs lg:text-sm">
+                    <td className="py-2 px-4 font-semibold">Age:</td>
+                    <td className="py-2 px-4">{profile.age}</td>
+                  </tr>
+                  <tr className="border-b text-xs lg:text-sm">
+                    <td className="py-2 px-4 font-semibold">Occupation:</td>
+                    <td className="py-2 px-4">{profile.occupation}</td>
+                  </tr>
+                  <tr className="border-b text-xs lg:text-sm">
+                    <td className="py-2 px-4 font-semibold">Experience:</td>
+                    <td className="py-2 px-4">{profile.experience}</td>
+                  </tr>
+                  <tr className="border-b text-xs lg:text-sm">
+                    <td className="py-2 px-4 font-semibold">Salary:</td>
+                    <td className="py-2 px-4">{profile.salary}</td>
+                  </tr>
+                  <tr className="border-b text-xs lg:text-sm">
+                    <td className="py-2 px-4 font-semibold">
+                      Current Address:
+                    </td>
+                    <td className="py-2 px-4">{profile.currentAddress}</td>
+                  </tr>
+                  <tr className="border-b text-xs lg:text-sm">
+                    <td className="py-2 px-4 font-semibold">
+                      Permanent Address:
+                    </td>
+                    <td className="py-2 px-4">{profile.permanentAddress}</td>
+                  </tr>
+                  <tr className="border-b text-xs lg:text-sm">
+                    <td className="py-2 px-4">
+                      <img
+                        src={profile.photo1}
+                        alt="Profile Photo 1"
+                        className="w-auto h-24 object-contain rounded"
+                      />
+                    </td>
+                    <td className="py-2 px-4">
+                      <img
+                        src={profile.photo2}
+                        alt="Profile Photo 2"
+                        className="w-auto h-24 object-contain rounded"
+                      />
+                    </td>
+                  </tr>
+                  <tr className="border-b text-xs lg:text-sm">
+                    <td className="py-2 px-4 font-semibold">Height:</td>
+                    <td className="py-2 px-4">{profile.height}</td>
+                  </tr>
+                  <tr className="border-b text-xs lg:text-sm">
+                    <td className="py-2 px-4 font-semibold">Message:</td>
+                    <td className="py-2 px-4">{profile.message}</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           )}
-          </div>
+        </div>
         <Footer />
       </div>
     </div>
--- src/components/Profiles/index.jsx
+++ src/components/Profiles/index.jsx
@@ -33,7 +33,7 @@ const Profiles = () => {
 
   const handleDeleteProfile = async (id) => {
     try {
-      await axios.delete(`http://localhost:5000/api/profiles/${id}`);
+      await axios.delete(`https://m-bureau-backend.onrender.com/api/profiles/${id}`);
       setProfiles(profiles.filter((profile) => profile._id !== id));
       setError(null);
       setDeleteDiv(false);
@@ -46,7 +46,9 @@ const Profiles = () => {
     const fetchProfiles = async () => {
       setLoading(true); // Start loading
       try {
-        const response = await axios.get("https://m-bureau-backend.onrender.com/api/profiles");
+        const response = await axios.get(
+          "https://m-bureau-backend.onrender.com/api/profiles"
+        );
         setProfiles(response.data);
       } catch (error) {
         setError("Error fetching profiles");
@@ -63,20 +65,29 @@ const Profiles = () => {
       <div className="h-full w-screen">
         <NavbarDemo />
         <div className="my-16 py-12">
-          <div className="h-full mb-2 flex items-center justify-around border border-slate-300 dark:bg-gray-900 rounded-lg py-4">
+          <div className="h-full mb-2 flex items-center justify-around flex-wrap border border-slate-300 dark:bg-gray-900 rounded-lg py-4">
             <p className="sm:text-xs lg:text-sm">
               Welcome:{" "}
               <strong className="text-green-500 sm:text-xs lg:text-sm">
                 {storedAdminName}
               </strong>
             </p>
-            <Link to="/admin-dashboard" className="bg-green-400 text-xs text-white px-4 py-1 rounded-full font-bold focus:outline-none mx-2">
+            <Link
+              to="/admin-dashboard"
+              className="bg-green-400 text-xs text-white px-4 py-1 rounded-full font-bold focus:outline-none mx-2"
+            >
               Dashboard
             </Link>
-            <Link to="/upload-profiles" className="bg-green-400 text-xs text-white px-4 py-1 rounded-full font-bold focus:outline-none mx-2">
+            <Link
+              to="/upload-profiles"
+              className="bg-green-400 text-xs text-white px-4 py-1 rounded-full font-bold focus:outline-none mx-2"
+            >
               Upload Profiles
             </Link>
-            <button onClick={handleLogout} className="bg-green-400 hover:bg-green-200 focus:outline-none text-white sm:text-xs lg:text-sm font-bold py-1 px-4 rounded-full mx-2">
+            <button
+              onClick={handleLogout}
+              className="bg-green-400 focus:outline-none text-white text-xs lg:text-sm font-bold py-1 px-4 rounded-full mx-2 my-4"
+            >
               Logout
             </button>
           </div>
@@ -84,17 +95,38 @@ const Profiles = () => {
           {loading ? ( // Display loading animation
             <div className="flex justify-center items-center h-64">
               <video className="w-32 h-32" autoPlay loop muted>
-                <source src="https://res.cloudinary.com/dhxtw97su/video/upload/f_auto:video,q_auto/v1/marriage-bureau/xuutviaplagriy1f4llj" type="video/webm" />
+                <source
+                  src="https://res.cloudinary.com/dhxtw97su/video/upload/f_auto:video,q_auto/v1/marriage-bureau/xuutviaplagriy1f4llj"
+                  type="video/webm"
+                />
                 Your browser does not support the video tag.
               </video>
             </div>
           ) : (
             <>
               {deleteDiv && (
-                <div className="scale-100 bg-white overflow-x-auto overflow-y-hidden p-4 fixed right-20 left-20 z-50 rounded-lg border">
-                  <h1 className="text-center text-green-400 sm:text-sm lg:text-2xl my-4">Are you sure you want to delete this profile?</h1>
-                  <button onClick={() => handleDeleteProfile(selectedProfileId)} className="bg-green-400 hover:bg-green-200 focus:outline-none text-white sm:text-xs lg:text-2xl font-bold py-1 px-4 rounded-full mr-16 my-8">Yes</button>
-                  <button onClick={() => setDeleteDiv(false)} className="bg-red-400 hover:bg-red-200 focus:outline-none text-white sm:text-xs lg:text-2xl font-bold py-1 px-4 rounded-full my-8">No</button>
+                <div className="bg-white w-1/2 p-2 mx-auto fixed right-20 left-20 z-50 rounded-lg border">
+                  <h1 className="text-center text-green-400 text-sm lg:text-2xl my-4">
+                    Are you sure you want to delete this profile?
+                  </h1>
+                  <div>
+                    <div className="w-1/2 inline-block text-xs lg:text-2xl font-bold">
+                      <button
+                        onClick={() => handleDeleteProfile(selectedProfileId)}
+                        className="bg-green-400 hover:bg-green-200 focus:outline-none text-white py-1 px-3 rounded-full my-8"
+                      >
+                        Yes
+                      </button>
+                    </div>
+                    <div className="w-1/2 inline-block text-xs lg:text-2xl font-bold">
+                      <button
+                        onClick={() => setDeleteDiv(false)}
+                        className="bg-red-400 hover:bg-red-200 focus:outline-none text-white py-1 px-3 rounded-full my-8"
+                      >
+                        No
+                      </button>
+                    </div>
+                  </div>
                 </div>
               )}
               <div className="h-full my-12 overflow-x-auto overflow-y-hidden">
@@ -105,28 +137,57 @@ const Profiles = () => {
                       <th className="border px-4 py-2">Age</th>
                       <th className="border px-4 py-2">Occupation</th>
                       <th className="border px-4 py-2">Address</th>
-                      <th className="border px-4 py-2" colSpan="2">Actions</th>
+                      <th className="border px-4 py-2" colSpan="2">
+                        Actions
+                      </th>
                     </tr>
                   </thead>
                   <tbody>
                     {profiles.length > 0 ? (
                       profiles.map((profile) => (
-                        <tr key={profile._id} className="hover:bg-slate-300 hover:text-white">
-                          <td className="border px-4 py-2 text-xs">{profile.fullName}</td>
-                          <td className="border px-4 py-2 text-xs">{profile.age}</td>
-                          <td className="border px-4 py-2 text-xs">{profile.occupation}</td>
-                          <td className="border px-4 py-2 text-xs">{profile.currentAddress}</td>
+                        <tr
+                          key={profile._id}
+                          className="hover:bg-slate-300 hover:text-white"
+                        >
                           <td className="border px-4 py-2 text-xs">
-                            <Link to={`/profile/${profile._id}`} className="text-xs text-red-400 underline rounded font-bold focus:outline-none">View</Link>
+                            {profile.fullName}
                           </td>
                           <td className="border px-4 py-2 text-xs">
-                            <Link to="#" onClick={() => handleDelete(profile._id)} className="text-xs text-red-400 underline rounded font-bold focus:outline-none">Delete</Link>
+                            {profile.age}
+                          </td>
+                          <td className="border px-4 py-2 text-xs">
+                            {profile.occupation}
+                          </td>
+                          <td className="border px-4 py-2 text-xs">
+                            {profile.currentAddress}
+                          </td>
+                          <td className="border px-4 py-2 text-xs">
+                            <Link
+                              to={`/profile/${profile._id}`}
+                              className="text-xs text-red-400 underline rounded font-bold focus:outline-none"
+                            >
+                              View
+                            </Link>
+                          </td>
+                          <td className="border px-4 py-2 text-xs">
+                            <Link
+                              to="#"
+                              onClick={() => handleDelete(profile._id)}
+                              className="text-xs text-red-400 underline rounded font-bold focus:outline-none"
+                            >
+                              Delete
+                            </Link>
                           </td>
                         </tr>
                       ))
                     ) : (
                       <tr>
-                        <td colSpan="5" className="border px-4 py-2 text-center">No profiles found</td>
+                        <td
+                          colSpan="5"
+                          className="border px-4 py-2 text-center"
+                        >
+                          No profiles found
+                        </td>
                       </tr>
                     )}
                   </tbody>
